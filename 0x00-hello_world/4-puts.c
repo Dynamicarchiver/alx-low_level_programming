b4c626160df43c981bd1a5935249695ea7331571
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-09 05:21:24 -0800

    repeat task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 /*
+ * main - Entry point
  *
- * main - main block
- *
- * Return: 0
+ * Return: Always 0 (success)
  *
  */
 int main(void)
-
 { 
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
