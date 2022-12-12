338135e1899caf4a0f5b02536df0c82a85dc9855
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-11 21:03:49 -0800

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 'A - Z';
+	int result = tolower(c);
+	putchar("%d\n", result);
+}
