1ab39669d9a7d1a4c435d56f254614a9fff7e450
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-12 17:42:25 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= z)
+	{
+		if (c = 'q' || c = 'e')
+		{
+			c = c + 2;
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+		putchar('\n');
+
+		Return (0);
+}
