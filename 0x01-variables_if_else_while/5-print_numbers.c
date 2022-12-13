4d02470dc4260599cac20e0f6f8640e6e4573c2a
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-12 18:32:43 -0800

    task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (0 <= 9)
+	{
+		printf("%d\n",a);
+		a++;
+	}
+}
