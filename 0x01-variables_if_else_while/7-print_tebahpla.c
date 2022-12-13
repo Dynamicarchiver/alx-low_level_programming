0ed5e2d300c12e271cd601583bd3fb65cbbbc35b
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-12 18:56:41 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while (a >= 'a')
+	{
+		putchar(a);
+	}
+	return (0);
+}
