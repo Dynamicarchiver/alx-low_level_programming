02c9a6e11b7b92223bc9b4bc95d6dad57b0c9106
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-12 19:11:07 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int a;
+
+	while(a < 16)
+	{
+		if (a < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
