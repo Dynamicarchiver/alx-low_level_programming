2149111216047392810f88ed6f67f72ca180080f
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 05:05:28 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @a: number to be read
+ * return: the absolute value of number
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (n * (-1));
+	}
+	else
+	{
+		return (n);
+	}
+}
