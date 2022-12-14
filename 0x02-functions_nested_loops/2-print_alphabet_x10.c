0c7c923c49f1bc4524dbf48cecc46805ee479a3d
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 03:55:35 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return : 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+		while ( a <= 10)
+		{
+			a++;
+		}
+		putchar('\n);
+	}
+
+}
