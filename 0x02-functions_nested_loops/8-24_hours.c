7e1cf81f839ebfc932cdc87e32db27825f486610
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 06:04:41 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ void jack_bauer(void)
 {
 	int h;
 	int m;
-	
+
 	h = 0;
 	while (h < 24)
 	{
 		m = 0;
-		while ( m < 60)
+		while (m < 60)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
