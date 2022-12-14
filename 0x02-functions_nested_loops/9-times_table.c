eb860d495b4351afb8ef20a85f921dd78bf339f2
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 08:00:46 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,13 +22,13 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 			}
-			 else
-			 {
+			else
+			{
 				_putchar(' ');
-			 }
+			}
 			 _putchar(c % 10 + '0');
 			b++;
-			if (b < 10)
+			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
