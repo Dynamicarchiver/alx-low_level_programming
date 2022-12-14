d25a1b13ee4f79221151b071200e71a85ebef52e
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 12:21:17 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,28 +12,28 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		while (n <= 98)
-	{
+		{	
 		printf("%d", n);
-	}
 		if (n != 98)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
 		n++;
+		}
 	}
 	else if (n > 98)
 	{
 		while (n >= 98)
 		{
 			printf("%d", n);
-		}
 		if (n != 98)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
 		n--;
+		}
 	}
 	else
 	{
