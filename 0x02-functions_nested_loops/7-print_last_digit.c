7a8a2d4c3c95892b177a89d95ad1cc62e68bd9ec
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-14 05:16:26 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @a: the number to be read
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int a)
+{
+	return ( a % 10);
+}
