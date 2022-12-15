e20bb589a3f0d3d50c95a291cd9ef8f3067e5668
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 09:36:10 -0800

    task 5

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * mul - Entry point
- * @b: first number to multiply
- * @c: second number to multiply
- * Return: result of the multiplication of a and b
+ * @: first input
+ * @b: second input
+ * * Return: multiplication of a and b
  */
 
 int mul(int a, int b)
