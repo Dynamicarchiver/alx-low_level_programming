74fd3a609e76dac96249765f69fb4e1539a06483
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 12:00:33 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number of spacess
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	if (a <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
