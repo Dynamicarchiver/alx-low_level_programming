e7d70f94d8cdbb46e928561bdbc812d2462c088a
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 11:33:47 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,14 @@
 /**
  * print_line - Entry point
  * @n: number of line to draw
- * Return: 0
+ * Return: a straight line
  */
 
 void print_line(int n)
 {
 	int a;
 
-	if(n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
