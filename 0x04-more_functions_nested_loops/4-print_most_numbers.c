69969bc5e9f61ccfcd24c9cbd63929d32f683ca5
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 09:20:23 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -16,6 +16,6 @@ void print_most_numbers(void)
 			a++;
 		}
 		_putchar(a + '0');
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: 0
+ */
+
