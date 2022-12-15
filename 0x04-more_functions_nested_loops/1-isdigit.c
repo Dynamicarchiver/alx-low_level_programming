f0c9906138e1f661802839cbceacb74ccd67c457
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 07:45:35 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: number to check
+ * Return: if c is a digit 1 else 0
+ */
+int _isdigit(int c);
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
