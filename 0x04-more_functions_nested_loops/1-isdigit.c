a53572810bef7867d987ec1ff5bc47ae653d6ee9
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 08:10:54 -0800

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,10 @@
  * @c: number to check
  * Return: if c is a digit 1 else 0
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
 
-	if (c >= 48 && c <= 57)
+	if ((c >= 48) && (c <= 57))
 		return (1);
 	else
 		return (0);
