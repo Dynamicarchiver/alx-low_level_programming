abf58bfb7359130412eae5fad38a731f18c50bf7
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-15 09:25:10 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,4 +5,18 @@
  *
  * Return: 0
  */
+void more_numbers(void)
+{
+	int a;
+	int b;
 
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
