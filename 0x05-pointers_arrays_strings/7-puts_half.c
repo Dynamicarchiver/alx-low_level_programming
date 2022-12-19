06bd47da7c0340052447e7c02373e1a6bf65d123
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-19 04:20:20 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * @str: pointer
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int length;
+	int n;
+
+	length = _strlen(str);
+	n = length / 2;
+	if (length % 2 = 1)
+		n = (length -1) / 2;
+	for (i = n; str[i] >=  length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
