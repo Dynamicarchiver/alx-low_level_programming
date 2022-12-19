25e70220d2bb3b4ffb0feeca45dbb7d507702bfc
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-19 01:12:45 -0800

    task 1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strlen - Entry point
  * @s: pointer
@@ -6,5 +7,5 @@
  */
 int _strlen(char *s)
 {
-	*s[] = (sixeof(*s) - 1);
+	*s = (sixeof(*s) - 1);
 }
