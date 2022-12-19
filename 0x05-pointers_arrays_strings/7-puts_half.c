0064986b30727bbd7d701e97ae2827263c2c3376
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-19 04:21:07 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 
 	length = _strlen(str);
 	n = length / 2;
-	if (length % 2 = 1)
+	if (length % 2 == 1)
 		n = (length -1) / 2;
 	for (i = n; str[i] >=  length; i++)
 		_putchar(str[i]);
