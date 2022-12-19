5a4cfa4b73e4918b956dd2cb797711467cefa1be
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-19 03:42:58 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i = i + 2)
+	for (i = 0; str[i] != '\'; i = i + 2)
 	{
 		_putchar(str[i]);
-		if (str[i] == '\0')
-		{
-			
-		}
 	}
 	_putchar('\n');
 }
