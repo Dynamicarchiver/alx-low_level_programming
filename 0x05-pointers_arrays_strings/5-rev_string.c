10cf66687b94dd1fff8477f76804788e2b2c3c54
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-19 03:05:05 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: string to print
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j = 0;
+	int k;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		j++;
+	}
+	for (k = j - 1; k >= 0; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
