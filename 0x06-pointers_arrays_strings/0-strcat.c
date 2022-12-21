72135d1cf486e2ef80b23c75278ea1edbc7db4e3
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2022-12-21 12:58:30 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+ * @dest: first pointer
+ * @src: second ponter
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	for (; *dest != '\0'; dest++)
+	{
+		_putchar(*dest);
+	}
+	for (; *src != '\0'; src++)
+	{
+		*dest++ = _putchar(*src);
+	}
+	*dest = '\0';
+
+}
