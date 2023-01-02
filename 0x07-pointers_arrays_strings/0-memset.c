9037e0bc7fd6d646f97af308ac2e20e9fa87a4f6
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2023-01-02 06:32:17 -0800

    task 0 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memeset - a function that replace b from s in a count of n in a n array
+ * _memset - A function that replace b from s in a count of n in a n array
  * @s: where to start replace from
  * @b: value to be replaced with
  * @n: count or iteration at which the value of b os replaced
