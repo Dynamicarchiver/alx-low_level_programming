28b51c63b91359ed602d5ce1dc4ed6712ee381c9
Boluwatife Adeyemi <boluwatifeadeyemi1226@gmail.com>
2023-01-02 04:11:11 -0800

    task 0 - memeset function

---
+++ 0x07-pointers_arrays_strings/memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memeset - a function that replace b from s in a count of n in a n array
+ * @s: where to start replace from
+ * @b: value to be replaced with
+ * @n: count or iteration at which the value of b os replaced
+ * Return: a pointer to the memeory area
+ */
+
+char *_memeset(char *s, char b, unsigned int n)
+{
+	int i;
+	/*type castiing *s from void to a char*/
+	char *st = (char *) s;
+
+	/*filling n element with b*/
+	for (i = 0; i < n; i++)
+		st[i] = b;
+}
