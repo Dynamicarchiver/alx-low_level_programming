40fa7195d512ae797bcb0b51b4286784c91be123
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-13 11:56:13 -0800

    Advanced task 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,27 +10,21 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			if (a < b)
 			{
-				for (d = 0; d <= 9; d++)
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98)
 				{
-					if (a <= c && (a + b) < (c + d))
-					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
-					if (b < 8)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
