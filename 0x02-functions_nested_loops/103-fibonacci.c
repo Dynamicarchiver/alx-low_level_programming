5c5a936b95904f0a1f4fb2b6d72dbf62c33fa2de
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-14 07:02:14 -0800

    task 3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, sum;
+
+	b = 0;
+	c = 1;
+	sum = 0;
+	for (a = 0; (sum) <= 4000000; a++)
+	{
+		sum = b + c;
+		if ((sum % 2) == 0)
+		{
+			printf("%ld\n");
+		}
+		b = c;
+		c = sum;
+	}
+
+}
