3faef386669dd9d8c1796abf39a6c47e83ac477b
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-14 01:05:53 -0800

    advanced task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "maih"
+#include <stdio.h>
+
+/**
+ * print_times_table - A functio that print time table of n 
+ * @n: numbers of times to print timetable
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+
+				printf("%d", c);
+				if (b < n)
+				{
+					d = a * (b + 1);
+
+					if (d < 10)
+					{
+						printf(",   ");
+					}
+					else if(d < 100)
+					{
+						printf(",  ");
+					}
+					else
+					{
+						printf(", ");
+					}
+				}
+				else
+				{
+					printf("\n");
+				}
+			}
+		}
+	}
+	printf("\n");
+}
