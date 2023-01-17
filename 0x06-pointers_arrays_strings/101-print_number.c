a1f30e82a65a8d6143f57dc11e258388c872d77b
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-17 03:07:32 -0800

    advanced task 1

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - a function to print a number
+ * @n: the number to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	
+	if (num / 10 > 0)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar(num % 10 + '0');
+}
