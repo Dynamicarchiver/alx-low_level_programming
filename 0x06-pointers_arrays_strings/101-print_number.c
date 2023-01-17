b1a92561a4cb8277893340364a811a4594175c1d
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-17 03:12:31 -0800

    advanced task 2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,14 @@
 
 void print_number(int n)
 {
-	unsigned num = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	
+
 	if (num / 10 > 0)
 	{
 		print_number(num / 10);
