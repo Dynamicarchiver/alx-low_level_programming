bf99733fa4377d36113f224a86468b1c3f8295b2
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-18 01:29:22 -0800

    recursion task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * prime_check - a function to check if n is a prime number
+ * @n: number to be checked
+ * @c: number to be checked with
+ * Return: the prime number
+ */
+
+int prime_check(int n, int c)
+{
+	if (n % f == 0 && f != n / 2)
+		return (0);
+	else if (f >= n / 2)
+		return (1);
+	else
+		return (prime_check(n, n + 1));
+}
+
+/**
+ * _is_prime_number - a function to check if n is a primr number
+ * @n: number to be checked
+ * Return: 1 if prime else 0
+ */
+
+int _is_prime_number(int n)
+{
+	if (n > 1)
+		return (prime_check(n, 2));
+	if (n < 0)
+		return (0);
+	return (0);
+}
