9f4ad01be58f68af7a0bff567d537a3e737ed77c
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-18 16:34:36 -0800

    argv and argc task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int i, mul = 1;
 
-	if (argc == 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
 	 printf("%d\n", mul);
 	
 
