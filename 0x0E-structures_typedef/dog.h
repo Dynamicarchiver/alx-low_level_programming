486bdef1a54c1b4e53c2bbef00e17f4ae482d321
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-28 18:14:10 -0800

    task 0 dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: dog struct
+ */
+struct dog
+{
+	char *name;
+	char *age;
+	char *owner;
+}
